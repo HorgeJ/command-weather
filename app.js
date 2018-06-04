c5a8683b69f318664ca09f30e255d59b6bf9535f
Jorge Sagastume <HorgeJ@users.noreply.github.com>
2018-06-04 00:56:27 -0700

    inital commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// Gets weather information based on query information
+const weather = require("./weather");
+
+const query = process.argv.slice(2).join("_").replace(' ', '_');
+
+weather.get(query);
---
+++ weather.js
@@ -0,0 +1,24 @@
+const https = require('https');
+const api = require('./api.json');
+
+function printWeather(weather){
+    const message = `Current weather in ${weather.location.city} is ${weather.current_observation.temp_f}F`;
+    console.log(message);
+}
+
+function get(query){
+    const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json`,
+        response => {
+            let body = "";
+            // Read data from responding API
+            response.on('data', chunk => {
+                body += chunk;
+            });
+            response.on('end', () => {
+                const weather = JSON.parse(body);
+                printWeather(weather);
+            });
+        });
+}
+
+module.exports.get = get;
