99c659f8a8235ad4f77f36ba3207271677c0cd61
Jorge Sagastume <HorgeJ@users.noreply.github.com>
2018-06-04 01:45:19 -0700

    comments

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ const weather = require("./weather");
 
 const query = process.argv.slice(2).join("_").replace(' ', '_');
 
-weather.get(query);
+weather.get(query); // get weather information, and print to console
--- weather.js
+++ weather.js
@@ -7,18 +7,18 @@ function printWeather(weather){
 }
 
 function get(query){
-    const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json`,
+    const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json`, // API query
         response => {
-            let body = "";
+            let body = "";      // temp var, holds data
             // Read data from responding API
             response.on('data', chunk => {
                 body += chunk;
             });
             response.on('end', () => {
-                const weather = JSON.parse(body);
-                printWeather(weather);
+                const weather = JSON.parse(body);   // parse information
+                printWeather(weather);              // call weather function w/location info
             });
         });
 }
 
-module.exports.get = get;
+module.exports.get = get;                           // export method to app.js
